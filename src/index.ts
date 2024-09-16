64b4a6867f16aeeeed273a4119530fa8924b841a
CAMILLA ELIZABETH RODRIGUES BARROS <CAMILLA3405@IOT>
2024-09-16 16:32:10 -0300

    Criando o projeto

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+class Produto {
+    private _preco: number = 0;
+    private _imageUrl: string = "";
+    private _category: string = "";
+    private _productTile: string = "";
+
+
+    constructor (preco: number, category: string, title:string) {
+        this._category = category,
+        this._preco = preco,
+        this._productTile = title
+    }
+
+    
+
+}
