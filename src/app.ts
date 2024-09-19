05d6cba499c0a27bac69bcf461a208436e9f81fb
Camilla Barros <camillaerbarros@gmail.com>
2024-09-18 21:02:47 -0300

    renderizando produtos com dados

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,15 @@
 // importar os dados
 import data from "../data.json"; // não usou chaves pq é dado bruto e não exporta nada
-// importar product.ts
+// importar a entidade product.ts
 import { Product } from "./entities/products";
-
-
 
 // criar os objetos usando os dados
 for (let i = 0; i < data.length; i++) {
-    const nome = data[i].name
-    const category = data[i].category
-    const price = data[i].price
-    const imageUrl = data[i].image.desktop
-    const product = new Product(nome, category, price, imageUrl)
-    console.log(product);
+  const nome = data[i].name;
+  const category = data[i].category;
+  const price = data[i].price;
+  const imageUrl = data[i].image.desktop;
+  const product = new Product(nome, category, price, imageUrl);
 
-    product.render()
+  product.renderProducts();
 }
--- src/entities/products.ts
+++ src/entities/products.ts
@@ -1,47 +1,83 @@
 import { v4 as uuid4 } from "uuid";
 
 export class Product {
-    private _id: string = uuid4();
-    private _name: string = "";
-    private _category: string = "";
-    private _price: number = 0;
-    private _imageUrl: string = "";
+  private _id: string = uuid4();
+  private _name: string = "";
+  private _category: string = "";
+  private _price: number = 0;
+  private _imageUrl: string = "";
 
+  constructor(name: string, category: string, price: number, imageUrl: string) {
+    (this._category = category),
+      (this._price = price),
+      (this._name = name),
+      (this._imageUrl = imageUrl);
+  }
 
-    constructor (name:string, category: string, price: number, imageUrl: string) {
-        this._category = category,
-        this._price = price,
-        this._name = name,
-        this._imageUrl = imageUrl
+  renderProducts() {
+    // cria o container principal
+    const productCard = document.createElement("div");
+    productCard.className = "product-card";
+
+    // imagem
+    const productImage = document.createElement("div");
+    productImage.className = "product-image";
+    productImage.innerHTML = `<img
+                src="${this._imageUrl}"
+                alt="${this._name}"
+              />`;
+
+    // botão add cart
+    const addCartBtn = document.createElement("div");
+    addCartBtn.className = "add-cart-btn";
+    addCartBtn.innerHTML = `<div class="add-cart-icon">
+                <i class="fa fa-cart-plus" aria-hidden="true"></i>
+              </div>
+              <span>Add to Cart</span>`;
+
+    // informações
+    const productInfo = document.createElement("div");
+    productInfo.className = "product-information";
+
+    // categoria
+    const productCat = document.createElement("div");
+    productCat.className = "product-category";
+    productCat.innerHTML = `<span>${this._category}</span>`;
+
+    // nome do produto
+    const productName = document.createElement("div");
+    productName.className = "product-name";
+    productName.innerHTML = `<span>${this._name}</span>`;
+
+    // preço do produto
+    const productPrice = document.createElement("div");
+    productPrice.className = "product-price";
+    productPrice.innerHTML = `<span>$ ${this._price}</span>`;
+
+    // adiciona as informações a div de informações
+    if (productInfo) {
+      productInfo.appendChild(productCat);
+      productInfo.appendChild(productName);
+      productInfo.appendChild(productPrice);
     }
 
-    render() {
-        const containerPrincipal = document.createElement("div")
-        containerPrincipal.className = "container-principal"
-
-        const header = document.createElement('div')
-        header.className = 'header-product'
-        header.innerHTML = `<div class="header-products">
-          <h2>Desserts</h2>
-        </div>`
-
-        containerPrincipal.append(header)
-    
-        const mainContainer = document.getElementById('main-id')
-        if (mainContainer) {
-            mainContainer.appendChild(containerPrincipal)
-        }    
+    // pega o container principal e adiciona o cards
+    const cards = document.getElementById("cards");
+    if (cards) {
+      cards.appendChild(productCard);
     }
 
-    add() {
-        return this._id;
-    }
+    // adiciona os 3 componentes do card ao card
+    productCard.append(productImage, addCartBtn, productInfo);
+  }
 
-    remove() {
-        console.log("olá")
-    }
-    
+  //   add() {
+  //     return this._id;
+  //   }
 
+  //   remove() {
+  //     console.log("olá");
+  //   }
 }
 
 // const produto = new Product(20, "comida","pizza", "dvgfslghjrf");
